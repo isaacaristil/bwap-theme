6359f0d8e88224da91d183cad217bec85d52b505
alanpilloud <alanp@rc2.ch>
2016-02-26 13:47:00 +0100

    require admin functions for admin only

--- functions.php
+++ functions.php
@@ -1,9 +1,13 @@
 <?php
 require('functions/theme-init.php');
 
-if(current_user_can('editor')) {
-	require('functions/backend-editor.php');
+if (is_admin()) {
+    if (current_user_can('editor')) {
+    	require('functions/backend-editor.php');
+    }
+    require('functions/backend.php');
+} else {
+    require('functions/frontend.php');
 }
-require('functions/backend.php');
-require('functions/frontend.php');
+
 require('functions/functions.php');
