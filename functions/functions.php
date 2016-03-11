97ca53ce83dd2aa4c1340e8dd30c617dbc7d6927
alanpilloud <alanp@rc2.ch>
2016-03-11 16:04:12 +0100

    Added  post type example commented

--- functions/functions.php
+++ functions/functions.php
@@ -17,3 +17,28 @@ add_action('widgets_init', function() {
     unregister_widget('WP_Widget_Tag_Cloud');
     unregister_widget('WP_Nav_Menu_Widget');
 }, 1);
+
+/**
+ *  Add ACF options page
+ */
+//acf_add_options_page();
+
+/**
+ *  Registering post types
+ */
+/*register_post_type('[POSTTYPE]', array(
+    'label'                 => '[POSTTYPEs]',
+    'labels'                => array(
+        'name'                  => '[POSTTYPEs]',
+        'singular_name'         => '[POSTTYPE]',
+    ),
+    'supports'              => array( 'title', 'editor', 'thumbnail', ),
+    'taxonomies'            => array( 'category' ),
+    'hierarchical'          => true,
+    'public'                => true,
+    'show_in_menu'          => true,
+    'show_in_admin_bar'     => false,
+    'has_archive'           => true,
+    'capability_type'       => 'page',
+    //'rewrite'               => array('slug' => '[MY_SLUG]')
+));*/
