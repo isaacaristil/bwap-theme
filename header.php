2c24df258d3a320ebc48afde2774b7a8d38e37c2
alanpilloud <alanpilloud@gmail.com>
2016-04-19 22:25:14 +0200

    title tag is no more define by default

--- header.php
+++ header.php
@@ -3,7 +3,13 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui" />
-	<title><?php wp_title(''); ?></title>
+	<?php
+    /*
+    If you are not useing the BWAP Title Tag Plugin, you should define a title here
+    
+    Plugin Url : https://github.com/alanpilloud/bwap-titletag
+    */
+    ?>
 	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png" />
 	<?php wp_head(); ?>
 </head>
