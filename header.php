2185f61356dbb1a24e7cb9adc5bae37754de2946
alanpilloud <alanp@rc2.ch>
2016-05-25 10:16:10 +0200

    Add language attribute to html

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui" />
 	<?php
     /*
     If you are not useing the BWAP Title Tag Plugin, you should define a title here
-    
+
     Plugin Url : https://github.com/alanpilloud/bwap-titletag
     */
     ?>
